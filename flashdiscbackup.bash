f46d19d88d48ebeda3b9779838066bbac3ff87fb
baetm <etmun86@gmail.com>
2022-06-16 15:56:57 +0200

    Initial commit

---
+++ flashdiscbackup.bash
@@ -0,0 +1,26 @@
+#! /bin/bash
+
+# ---------------------------------------------------------------------
+# FlashDiscBackup - Bash shell script to make backup for important 
+# data and store in pendrive, memory card or disc
+
+# Copyright 2022, Bartosz Etmanski
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License at <http://www.gnu.org/licenses/> for
+# more details.
+
+# Usage: TO DO
+
+# Revision history
+# 2022-06-16 Created
+# ---------------------------------------------------------------------
+
+
