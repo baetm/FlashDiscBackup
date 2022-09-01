e620af9d74a5c1460e9d9606b97e4428a7cb9c6d
baetm <etmun86@gmail.com>
2022-09-01 20:57:06 +0200

    Add missing comment in section USAGE of the description.

--- flashdiscbackup.bash
+++ flashdiscbackup.bash
@@ -18,7 +18,37 @@
 # more details.
 
 # Usage: 
-# README.md has description.
+# To run the script you should download the project from the GitHub page and move to
+# destination place. Next, you should check the name of the flashdisc. In this
+# case, you should check the mount catalogue. For example, for Ubuntu will
+# be the /media/name-of-user/. If you find the name of the flashdisc
+# then it should open flashdiscbackup.bash file, find in the VARIABLES
+# section disc_name variable. After the = should add the name of
+# flashdisc and check the path of the mount.
+#
+# flashdisc_path=/media/$USER
+#
+# disc_name=name-of-flashdisc
+# 
+# After that, there should be a prepared list of the absolute paths to backup on
+# the flashdisc in any text editor e. g.
+# 
+# /home/user-name/Documents/
+# /home/user-name/Music/
+#
+# The name of the list of backup catalogues should be added to VARIABLE
+# section in flashdiscbackup.bash (name_backup_file_list) in the same
+# way like flashdisc before. The list has to be in the same catalogue as a script file..
+# 
+# When you prepare the list file and add all names, the next step is to add
+# the privilege of running on your system in the next command in a terminal 
+# emulator:
+#
+# $ sudo chmod +x flashdiscbackup.bash
+#
+# and then it may run the script in command:
+# 
+# $ ./flashdiscbackup.bash
 
 # Revision history
 # 2022-06-16 Created
